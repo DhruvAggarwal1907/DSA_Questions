3b91bc18c25d63af4cddc57ea797aa556fa5a9c6
Dhruv Aggarwal <104718579+DhruvAggarwal1907@users.noreply.github.com>
2023-08-19 15:18:14 +0530

    Add files via upload

---
+++ Arrays/Move_Zeroes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of the Array: ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int i=0;
+    for(int j=0;j<n;j++){
+        if(arr[j] != 0){
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
