b53b7e79ef141a7dabf11b738341be953934c278
Dhruv Aggarwal <104718579+DhruvAggarwal1907@users.noreply.github.com>
2023-08-29 18:54:12 +0530

    Add files via upload

---
+++ Arrays/Reverse_Array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of the Array: ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int start=0, end=n-1;
+    int temp=0;
+
+    while(start<end){
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+        }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
